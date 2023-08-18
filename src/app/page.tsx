30cf9341fe950a900f0cda0d64d8731bd4f75225
Talha Imran <talhaimran284@gmail.com>
2023-08-18 22:52:22 +0500

    Added hero section for home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <div className="px-2 min-[1208px]:px-0">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,16 @@
+const HeroSection = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto flex justify-center items-center my-[265px]">
+      <div className="font-bold leading-[70px] text-[#42446E] text-center">
+        <h1 className="text-5xl mb-8">
+          Hi👋, My name is<span className="text-[#7367D3]"> Talha Imran</span><br/>
+        </h1>
+        <h1 className="text-5xl">
+          I build things for web.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
