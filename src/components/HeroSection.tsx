daa79ec00179c6dd1737e526281ef80f41abeab2
Talha Imran <talhaimran284@gmail.com>
2023-08-21 21:05:45 +0500

    Updated hero section & logo text.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,26 +4,22 @@ import ProfilePic from "images/profile-pic.jpg";
 const HeroSection = () => {
   return (
     <div className="my-[50px] lg:my-[100px] flex flex-col justify-center items-center">
+      <div className="flex justify-center items-center mb-16">
+        <div className="font-bold leading-[70px] text-[#42446E] text-center">
+          <h1 className="text-3xl lg:text-5xl lg:mb-6 font-black">
+            Frontend Web Developer
+          </h1>
+          <p className="text-3xl lg:text-2xl font-light">
+            I code beautifully simply things, and I love what i do.
+          </p>
+        </div>
+      </div>
       <Image
-        className="h-52 w-52 lg:h-80 lg:w-80 rounded-full border-4 border-[#7367D3] mb-10"
+        className="h-52 w-52 rounded-full border-4 border-[#7367D3]"
         src={ProfilePic}
         alt="Profile Image"
         placeholder="blur"
       />
-      <div className="flex justify-center items-center ">
-        <div className="font-bold leading-[70px] text-[#42446E] text-center">
-          <h1 className="text-3xl lg:text-5xl lg:mb-8">
-            Hi👋,
-            <br /> My name is
-            <br />
-            <span className="text-[#7367D3] text-4xl lg:text-6xl">
-              Talha Imran
-            </span>
-            <br />
-          </h1>
-          <h1 className="text-3xl lg:text-5xl">I build things for web.</h1>
-        </div>
-      </div>
     </div>
   );
 };
