44f0f0a9e403c417b662598c4c4482874c8f06eb
Talha Imran <talhaimran284@gmail.com>
2023-08-22 21:41:28 +0500

    Added developer portfolio project.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import Card from "components/ui/Card";
 import SectionInto from "components/ui/SectionInto";
 import MultiStepForm from "images/multi-step-form.png";
+import DeveloperPortfolio from "images/developer-portfolio.png";
 
 const Projects = () => {
   return (
@@ -14,10 +15,19 @@ const Projects = () => {
           image={MultiStepForm}
           title="Multi Step Form"
           description="Crafted with Next.js, Context API, and Tailwind CSS, this multi-step form offers a seamless user experience. Simplify complex data collection in style."
-          technologies={["nextjs", "context api", "tailwindcss"]}
+          technologies={["nextjs", "context api", "tailwindcss", "react"]}
           livePreview="https://multi-step-form-n84yetjdt-talhaimran777.vercel.app"
           githubUrl="https://github.com/talhaimran777/multi-step-form-challenge"
         />
+        <Card
+          image={DeveloperPortfolio}
+          title="Devloper Portfolio Website"
+          description="Leveraging the latest in web development technologies, I utilized Next.js to build an optimized and visually appealing front-end interface that incorporates proper image and font optimizations. Further enhancing the user experience, I integrated animations using the Framer Motion library"
+          technologies={["nextjs", "framer motion", "tailwindcss", "react"]}
+          livePreview="https://single-page-devloper-portfolio-challenge.vercel.app"
+          githubUrl="https://github.com/talhaimran777/single-page-devloper-portfolio-challenge"
+        />
+
       </div>
     </div>
   );
